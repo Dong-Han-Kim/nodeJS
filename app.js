3c9c39fa912a970c8edb9e62ba6068caf3fde9a6
Dong-Han-Kim <dh32110@gmail.com>
2024-07-14 21:44:52 +0900

    express적용

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 
-function handleRequest(request, response) {
-	if (request.url === '/currenttime') {
-		response.statusCode = 200;
-		response.end('<h1>' + new Date().toISOString() + '</h1>');
-	} else if (request.url === '/') {
-		response.statusCode = 200;
-		response.end('<h1>Hello World</h1>');
-	}
-}
+app.get('/currenttime', function (req, res) {
+	res.send('<h1>' + new Date().toISOString() + '</h1>');
+});
+app.get('/', function (req, res) {
+	res.send('<h1>Hello World</h1>');
+});
 
-const server = http.createServer(handleRequest);
-server.listen(3000);
+app.listen(3000);
